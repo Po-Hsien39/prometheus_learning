7fc1a533fd3a02a2a11c9bd8e61576699abe2d03
tristan.wu <tristan.wu@linecorp.com>
2024-06-11 16:13:37 +0800

    Connect get & delete api

--- demo-app/src/Components/section.jsx
+++ demo-app/src/Components/section.jsx
@@ -1,92 +1,153 @@
 import { useState, useEffect } from "react";
 import TodoItem from "./todo_item";
+import axios from "axios";
 
-function Section(props){
-    const [todoItem, setTodoItem] = useState([])
-    const [input, setInput] = useState('')
-    const [todoNum, setTodoNum] = useState(0)
-    const [update, setUpdate] = useState(true)
-    const deleteNum = (num) => {
-        setTodoItem(todoItem.map(item=>{
-            if (item.num === num)
-                item.delete = !item.focus
-            return item
-    }))}
+function Section(props) {
+  const [todoItem, setTodoItem] = useState([]);
+  const [input, setInput] = useState("");
+  const [todoNum, setTodoNum] = useState(0);
+  const [update, setUpdate] = useState(true);
 
-    const editChecked = (num)=>{
-        setTodoItem(todoItem.map(item=>{
-            if (item.num === num)
-                item.focus = !item.focus
-            return item
-        }))
-        setUpdate(!update)
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/todos");
+      setTodoItem(response.data.map((d) => {
+        return {
+          num: d.Id,
+          value: d.task,
+          focus: false,
+          delete: false,
+          hide: false,
+        }
+      }))
+    } catch (error) {
+      console.error("Error fetching todos:", error);
     }
-    // Update how many tasks are left
-    useEffect(()=>{
-        let num = 0;
-        let rest = 0
-        todoItem.map((item)=>{
-            if (item.focus === false && item.delete === false)
-                num += 1;
-            if (!item.delete)
-                rest += 1;
+  };
+
+  const deleteTodo = async (num) => {
+    try {
+      await axios.delete('http://localhost:4000/todo', {
+        data: {id: num}
+      })
+    } catch (error) {
+      console.error("Error Deleting todos:", error);
+    }
+  }
+
+  const deleteNum = (num) => {
+    deleteTodo(num)
+    fetchTodos()
+    // setTodoItem(
+    //   todoItem.map((item) => {
+    //     if (item.num === num) item.delete = !item.focus;
+    //     return item;
+    //   })
+    // );
+  };
+
+  const editChecked = (num) => {
+    setTodoItem(
+      todoItem.map((item) => {
+        if (item.num === num) item.focus = !item.focus;
+        return item;
+      })
+    );
+    setUpdate(!update);
+  };
+
+  useEffect(() => {
+    fetchTodos()
+  }, []);
+
+  useEffect(() => {
+    let num = 0;
+    let rest = 0;
+    todoItem.map((item) => {
+      if (item.focus === false && item.delete === false) num += 1;
+      if (!item.delete) rest += 1;
+    });
+    props.editNum(num);
+    if (rest === 0) {
+      props.displayTask(false);
+    } else {
+      props.displayTask(true);
+    }
+  }, [todoItem]);
+
+  // Setting mode
+  useEffect(() => {
+    if (props.mode === "all") {
+      setTodoItem(
+        todoItem.map((item) => {
+          item.hide = false;
+          return item;
         })
-        props.editNum(num)
-        if (rest === 0){
-            props.displayTask(false)
-        }else{
-            props.displayTask(true)
-        }
-    }, [todoItem])
-
-    // Setting mode
-    useEffect(()=>{
-        if (props.mode === 'all'){
-            setTodoItem(todoItem.map((item)=>{
-                item.hide = false;
-                return item
-            }))
-        }else if (props.mode === 'active'){
-            setTodoItem(todoItem.map((item)=>{
-                (item.focus)?item.hide=true:item.hide=false
-                return item
-            }))
-        }else if (props.mode === 'delete'){
-            setTodoItem(todoItem.map((item)=>{
-                (item.focus)?item.delete=true:item.delete=item.delete
-                return item
-            }))
-        }else{
-            setTodoItem(todoItem.map((item)=>{
-                (!item.focus)?item.hide=true:item.hide=false
-                return item
-            }))
-        }
-    }, [props.mode, update])
-
-    const appendNewTodo = (e)=>{
-        if (e.key === "Enter"){
-            setTodoItem((oldArr)=>[...oldArr, {num: todoNum, value: input, focus: false, delete: false, hide: false}])
-            setTodoNum(todoNum + 1)
-            setInput('')
-        }
+      );
+    } else if (props.mode === "active") {
+      setTodoItem(
+        todoItem.map((item) => {
+          item.focus ? (item.hide = true) : (item.hide = false);
+          return item;
+        })
+      );
+    } else if (props.mode === "delete") {
+      todoItem.map((item) => {
+        item.focus ? (item.delete = true) : (item.delete = item.delete);
+        return item;
+      })
+    } else {
+      setTodoItem(
+        todoItem.map((item) => {
+          !item.focus ? (item.hide = true) : (item.hide = false);
+          return item;
+        })
+      );
     }
+  }, [props.mode, update]);
 
-    return (
-        <section className="todo-app__main">
-            <input value={input} onInput={e => setInput(e.target.value)} className="todo-app__input" placeholder="What needs to be done?" onKeyUp={appendNewTodo}/>
-            <ul className="todo-app__list" id="todo-list">
-                {todoItem.map((item)=>{
-                    return (<TodoItem key={item.num} 
-                        length={item.num} 
-                        value={item.value} 
-                        deleteNum={deleteNum} 
-                        modify={editChecked}
-                        status={item.hide || item.delete}/>)
-                })}
-            </ul>
-        </section>
-    )
+  const appendNewTodo = (e) => {
+    if (e.key === "Enter") {
+      setTodoItem((oldArr) => [
+        ...oldArr,
+        {
+          num: todoItem[-1].num + 1,
+          value: input,
+          focus: false,
+          delete: false,
+          hide: false,
+        },
+      ]);
+      setTodoNum(todoNum + 1);
+      setInput("");
+    }
+  };
+
+  return (
+    <section className="todo-app__main">
+      <input
+        value={input}
+        onInput={(e) => setInput(e.target.value)}
+        className="todo-app__input"
+        placeholder="What needs to be done?"
+        onKeyUp={appendNewTodo}
+      />
+      <ul className="todo-app__list" id="todo-list">
+        {todoItem.map((item) => {
+          return (
+            <TodoItem
+              key={item.num}
+              length={item.num}
+              value={item.value}
+              deleteNum={deleteNum}
+              modify={editChecked}
+              status={item.hide || item.delete}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
 
 export default Section;
--- demo-app/src/faro.ts
+++ demo-app/src/faro.ts
@@ -1,34 +1,37 @@
-import { FetchInstrumentation } from '@grafana/faro-instrumentation-fetch';
-import { XHRInstrumentation } from '@grafana/faro-instrumentation-xhr';
+import { FetchInstrumentation } from "@grafana/faro-instrumentation-fetch";
+import { XHRInstrumentation } from "@grafana/faro-instrumentation-xhr";
 import {
-    initializeFaro as coreInit,
-    getWebInstrumentations,
-} from '@grafana/faro-react';
-import { TracingInstrumentation } from '@grafana/faro-web-tracing';
-import type { Faro } from '@grafana/faro-react';
+  initializeFaro as coreInit,
+  getWebInstrumentations,
+} from "@grafana/faro-react";
+import { TracingInstrumentation } from "@grafana/faro-web-tracing";
+import type { Faro } from "@grafana/faro-react";
 
 export function initializeFaro(): Faro {
-    const instrumentationOptions = {
-        propagateTraceHeaderCorsUrls: [new RegExp('https://demo-be.cony.line-apps.com')], // This is a list of specific URIs or regular exprressions
-    };
-    const faro = coreInit({
-        url: 'http://localhost:8027/collect',
-        apiKey: 'api_key',
-        app: {
-            name: 'ocr-project',
-            version: '1.0.0', // Optional, but recommended
-        },
-        instrumentations: [
-            ...getWebInstrumentations({
-                captureConsole: true,
-            }),
-            new FetchInstrumentation({}),
-            new XHRInstrumentation({}),
-            new TracingInstrumentation({ instrumentationOptions }),
-        ],
-    });
+  const instrumentationOptions = {
+    propagateTraceHeaderCorsUrls: [
+      new RegExp("https://demo-be.cony.line-apps.com"),
+      new RegExp("http://localhost:4000"),
+    ], // This is a list of specific URIs or regular exprressions
+  };
+  const faro = coreInit({
+    url: "http://localhost:8027/collect",
+    apiKey: "api_key",
+    app: {
+      name: "ocr-project",
+      version: "1.0.0", // Optional, but recommended
+    },
+    instrumentations: [
+      ...getWebInstrumentations({
+        captureConsole: true,
+      }),
+      new FetchInstrumentation({}),
+      new XHRInstrumentation({}),
+      new TracingInstrumentation({ instrumentationOptions }),
+    ],
+  });
 
-    faro.api.pushLog(['Faro was initialized']);
+  faro.api.pushLog(["Faro was initialized"]);
 
-    return faro;
+  return faro;
 }
